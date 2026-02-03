532b594779cd66b1acf8b1760947697e0c859ffe
guntisratkevics <guntis.rat@gmail.com>
2026-02-03 23:24:22 +0200

    Sync ws-security: EXCL-C14N, WS-Addressing defaults, XAdES SignedProperties

--- latvian_einvoice/soap/client.py
+++ latvian_einvoice/soap/client.py
@@ -73,7 +73,7 @@ class SenderDocumentSigner(Plugin):
         signature_id = "SenderSignature"
         signature = xmlsec.template.create(
             envelope,
-            xmlsec.Transform.C14N,
+            xmlsec.Transform.EXCL_C14N,
             xmlsec.Transform.RSA_SHA512,
         )
         ref = xmlsec.template.add_reference(
@@ -153,9 +153,28 @@ class SignOnlySignature(Signature):
         self._signature_method = signature_method
         self._digest_method = digest_method
         with open(certfile, "rb") as fh:
-            self._cert_b64 = (
-                base64.b64encode(fh.read()).decode("ascii").replace("\n", "")
-            )
+            cert_bytes = fh.read()
+            self._cert_b64 = base64.b64encode(cert_bytes).decode("ascii").replace("\n", "")
+            self._cert_sha1_b64 = base64.b64encode(hashlib.sha1(cert_bytes).digest()).decode("ascii")
+            decoded = ssl._ssl._test_decode_cert(certfile)
+            issuer_parts = []
+            key_map = {
+                "commonName": "CN",
+                "domainComponent": "DC",
+                "organizationName": "O",
+                "organizationalUnitName": "OU",
+                "countryName": "C",
+            }
+            for rdn in reversed(decoded.get("issuer", [])):
+                for key, value in rdn:
+                    prefix = key_map.get(key, key)
+                    issuer_parts.append(f"{prefix}={value}")
+            self._issuer_name = ", ".join(issuer_parts)
+            serial_raw = decoded.get("serialNumber")
+            try:
+                self._serial_number = str(int(str(serial_raw), 16))
+            except Exception:
+                self._serial_number = str(serial_raw)
         self._add_timestamp = add_timestamp
         self._verify_response = verify_response
 
@@ -169,6 +188,7 @@ class SignOnlySignature(Signature):
         soap_env = detect_soap_env(envelope)
         if soap_env:
             security.set(QName(soap_env, "mustUnderstand"), "true")
+        body = envelope.find(QName(soap_env, "Body")) if soap_env else None
         for existing in list(security.findall(QName(ns.DS, "Signature"))):
             security.remove(existing)
 
@@ -192,6 +212,18 @@ class SignOnlySignature(Signature):
         to_el = envelope.find(f".//{{{ns.WSA}}}To")
         if to_el is not None:
             ensure_id(to_el)
+        else:
+            # Inject minimal WS-Addressing headers if missing
+            header = envelope.find(QName(soap_env, "Header")) if soap_env else None
+            if header is None:
+                header = etree.SubElement(envelope, QName(soap_env, "Header"))
+            to_el = etree.SubElement(header, QName(ns.WSA, "To"))
+            to_el.text = "http://vraa.gov.lv/div/uui/2011/11/UnifiedService"
+            ensure_id(to_el)
+            msg_id_el = etree.SubElement(header, QName(ns.WSA, "MessageID"))
+            msg_id_el.text = f"urn:uuid:{uuid.uuid4()}"
+            action_el = etree.SubElement(header, QName(ns.WSA, "Action"))
+            action_el.text = "http://vraa.gov.lv/div/uui/2011/11/IUnifiedService/SendMessage"
 
         bst_id = f"BST-{uuid.uuid4()}"
         bst = etree.Element(
@@ -225,6 +257,26 @@ class SignOnlySignature(Signature):
         )
         security.append(signature)
 
+        # XAdES SignedProperties (minimal BES)
+        xades_ns = "http://uri.etsi.org/01903/v1.3.2#"
+        qp = etree.Element(QName(xades_ns, "QualifyingProperties"), nsmap={"xades": xades_ns})
+        qp.set("Target", f"#{ensure_id(signature)}")
+        signed_props = etree.SubElement(qp, QName(xades_ns, "SignedProperties"))
+        ensure_id(signed_props)
+        ssp = etree.SubElement(signed_props, QName(xades_ns, "SignedSignatureProperties"))
+        signing_time = etree.SubElement(ssp, QName(xades_ns, "SigningTime"))
+        signing_time.text = _dt.datetime.utcnow().isoformat(timespec="seconds") + "Z"
+        sc = etree.SubElement(ssp, QName(xades_ns, "SigningCertificate"))
+        cert_el = etree.SubElement(sc, QName(xades_ns, "Cert"))
+        cert_digest = etree.SubElement(cert_el, QName(xades_ns, "CertDigest"))
+        etree.SubElement(cert_digest, QName(ns.DS, "DigestMethod"), Algorithm="http://www.w3.org/2000/09/xmldsig#sha1")
+        etree.SubElement(cert_digest, QName(ns.DS, "DigestValue")).text = self._cert_sha1_b64
+        issuer_serial = etree.SubElement(cert_el, QName(xades_ns, "IssuerSerial"))
+        etree.SubElement(issuer_serial, QName(ns.DS, "X509IssuerName")).text = getattr(self, "_issuer_name", "")
+        etree.SubElement(issuer_serial, QName(ns.DS, "X509SerialNumber")).text = getattr(self, "_serial_number", "")
+        obj = etree.SubElement(signature, QName(ns.DS, "Object"))
+        obj.append(qp)
+
         ctx = xmlsec.SignatureContext()
         key = xmlsec.Key.from_file(self.key_file, xmlsec.KeyFormat.PEM)
         key.load_cert_from_file(self.certfile, xmlsec.KeyFormat.PEM)
@@ -242,6 +294,18 @@ class SignOnlySignature(Signature):
             _add_reference(timestamp_el)
         if to_el is not None:
             _add_reference(to_el)
+        if action_el is not None:
+            _add_reference(action_el)
+        if msg_id_el is not None:
+            _add_reference(msg_id_el)
+        if body is not None:
+            _add_reference(body)
+        # SignedProperties reference
+        ref_props = xmlsec.template.add_reference(
+            signature, self._digest_method, uri="#" + ensure_id(signed_props)
+        )
+        ref_props.set("Type", "http://uri.etsi.org/01903#SignedProperties")
+        xmlsec.template.add_transform(ref_props, xmlsec.Transform.EXCL_C14N)
 
         ctx.sign(signature)
         return envelope, headers
@@ -264,6 +328,9 @@ class SoapClient:
             self.service = service
         else:
             transport = Transport(session=session, timeout=cfg.timeout)
+            # mTLS support if cert/key provided
+            if cfg.client_cert_path and cfg.client_key_path:
+                session.cert = (str(cfg.client_cert_path), str(cfg.client_key_path))
             wsse = None
             if cfg.wsse_signing and cfg.certificate and cfg.private_key:
                 # Signature expects key file first, certificate second
@@ -281,8 +348,9 @@ class SoapClient:
             if self._signer:
                 plugins.append(self._signer)
 
+            wsdl_url = cfg.local_wsdl_url or cfg.wsdl_url
             self._client = Client(
-                cfg.wsdl_url,
+                wsdl_url,
                 transport=transport,
                 plugins=plugins or None,
                 wsse=wsse,
@@ -290,7 +358,7 @@ class SoapClient:
             )
             # DIV test/prod bindings use the DIV namespace/binding name
             binding = "{http://vraa.gov.lv/div/uui/2011/11}UnifiedServiceHttpBinding_UnifiedServiceInterface"
-            self.service = self._client.create_service(binding, cfg.wsdl_url.replace("?wsdl", ""))
+            self.service = self._client.create_service(binding, wsdl_url.replace("?wsdl", ""))
         
         logger.info("Initialized e-adrese SOAP client @ %s", cfg.wsdl_url)
 
